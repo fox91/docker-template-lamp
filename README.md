2390061dc356c4cd4b7a4d90788c20d2168332e2
Andrea Falco <fox91fox@gmail.com>
2017-02-18 11:30:00 +0100

    Added bower

--- README.md
+++ README.md
@@ -5,12 +5,15 @@
 ```
 Docker Engine 1.13.0+
 Compose 1.10.0+
+bower
 ```
 
 ## Usage
 
 ```
 git clone https://github.com/fox91/docker-template-lamp.git my-project
-cd my-project
+cd my-project/my-web-app
+bower install
+cd ..
 docker-compose up
 ```
--- my-web-app/public/index.php
+++ my-web-app/public/index.php
@@ -1,3 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
 
-phpinfo();
+    <link href="/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <script src="/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
