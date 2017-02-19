eb0c89b8a33f458b3200cfadf9a9c8877550b2a7
Andrea Falco <fox91fox@gmail.com>
2017-02-19 16:35:00 +0100

    Added composer

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 ```
 Docker Engine 1.13.0+
 Compose 1.10.0+
+PHP 7.1+
+composer
 bower
 ```
 
@@ -13,6 +15,7 @@ bower
 ```
 git clone https://github.com/fox91/docker-template-lamp.git my-project
 cd my-project/my-web-app
+composer install
 bower install
 cd ..
 docker-compose up
--- my-web-app/public/index.php
+++ my-web-app/public/index.php
@@ -1,3 +1,36 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+function getMovies(
+  PDO $dbh
+) {
+  $movies = null;
+
+  try {
+    $sth = $dbh->prepare("SELECT `id`, `title` FROM `movies`");
+    $sth->execute();
+    foreach ($sth->fetchAll(PDO::FETCH_ASSOC) as $row) {
+      $movies[] = $row;
+    }
+  } catch(PDOException $e) {
+    echo $e->getMessage().PHP_EOL;
+  }
+
+  return $movies;
+}
+
+$dbh = new PDO(
+  'mysql:dbname='.getenv('MYSQL_ENV_MYSQL_DATABASE').';host='.getenv('MYSQL_PORT_3306_TCP_ADDR').';port='.getenv('MYSQL_PORT_3306_TCP_PORT'),
+  getenv('MYSQL_ENV_MYSQL_USER'),
+  getenv('MYSQL_ENV_MYSQL_PASSWORD')
+);
+
+$movies = getMovies($dbh);
+
+$dbh = null;
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -10,6 +43,27 @@
   </head>
   <body>
     <h1>Hello, world!</h1>
+
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody>
+<?php
+foreach ($movies as $movie) {
+?>
+        <tr>
+          <td><?php echo $movie['id']; ?></td>
+          <td><?php echo $movie['title']; ?></td>
+        </tr>
+<?php
+}
+?>
+      </tbody>
+    </table>
 
     <script src="/bower_components/jquery/dist/jquery.min.js"></script>
     <script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
